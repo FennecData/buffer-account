bd478f611ec6afb42ec01e53c20a5a9d093fe432
philippemiguet <philippe.miguet@gmail.com>
2017-11-27 14:49:23 +0100

    Build basic password reset component

--- packages/password-reset/components/PasswordReset/index.jsx
+++ packages/password-reset/components/PasswordReset/index.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import {Button, InputPassword} from '@bufferapp/components';
+import { Field, reduxForm } from 'redux-form';
 
-const PasswordReset = () =>
+const PasswordReset = ({onSaveClick}) =>
   <div>
-    Password Reset
+    <Field
+      name={'currentPassword'}
+      component={InputPassword}
+      label={'Current Password'}
+    />
+    <Field
+      name={'newPassword'}
+      component={InputPassword}
+      label={'New Password'}
+    />
+    <Field
+      name={'confirmPassword'}
+      component={InputPassword}
+    />
+
+    <Button onClick={onSaveClick}>Save</Button>
   </div>;
 
-export default PasswordReset;
+const formConfig = { form: 'password-reset' };
+export default reduxForm(formConfig)(PasswordReset);
--- packages/password-reset/components/PasswordReset/story.jsx
+++ packages/password-reset/components/PasswordReset/story.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
+import { action } from '@storybook/addon-actions';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
 import PasswordReset from './index';
+
+const store = createStore(combineReducers({ form: formReducer }));
 
 storiesOf('PasswordReset')
   .addDecorator(checkA11y)
+  .addDecorator(getStory =>
+    <Provider store={store}>
+      {getStory()}
+    </Provider>,
+  )
   .add('default', () => (
-    <PasswordReset />
+    <PasswordReset onSaveClick={action('saveButtonClick')} />
   ));
