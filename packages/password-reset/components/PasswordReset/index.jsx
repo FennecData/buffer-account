1deba39be0c35a0c48a7ef648e73653361297504
philippemiguet <philippe.miguet@gmail.com>
2017-11-27 17:36:01 +0100

    Add a bit of styling to PasswordReset

--- packages/password-reset/components/PasswordReset/index.jsx
+++ packages/password-reset/components/PasswordReset/index.jsx
@@ -3,23 +3,41 @@ import { Button, InputPassword } from '@bufferapp/components';
 import { Field, reduxForm } from 'redux-form';
 import { validate } from './validate.jsx';
 
+const currentPasswordStyle = {
+  marginBottom: '30px'
+}
+const newPasswordStyle = {
+  marginBottom: '10px'
+}
+const buttonStyle = {
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginTop: '30px'
+};
+
 const PasswordReset = ({handleSubmit, onSaveClick}) =>
   <form onSubmit={handleSubmit}>
-    <Field
-      name={'currentPassword'}
-      component={InputPassword}
-      label={'Current Password'}
-    />
-    <Field
-      name={'newPassword'}
-      component={InputPassword}
-      label={'New Password'}
-    />
+    <div style={currentPasswordStyle}>
+      <Field
+        name={'currentPassword'}
+        component={InputPassword}
+        label={'Current Password'}
+      />
+    </div>
+    <div style={newPasswordStyle}>
+      <Field
+        name={'newPassword'}
+        component={InputPassword}
+        label={'New Password'}
+      />
+    </div>
     <Field
       name={'confirmPassword'}
       component={InputPassword}
     />
-    <Button type={'submit'} onClick={onSaveClick}>Save</Button>
+    <div style={buttonStyle}>
+      <Button type={'submit'} onClick={onSaveClick}>Save</Button>
+    </div>
   </form>;
 
 const formConfig = { form: 'password-reset', validate };
